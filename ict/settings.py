c86693050d32a48d2780b0ef41088731fcf50172
Alexander <akurmazov2001@gmail.com>
2020-10-31 23:12:03 +0300

    Fix allowed hosts

--- ict/settings.py
+++ ict/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['localhost', 'ict-hack-api.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'ict-hack-api.herokuapp.com']
 
 
 # Application definition
