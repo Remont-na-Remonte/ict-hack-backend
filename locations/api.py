a8de9d270aecf42458778237548539d584f09095
Alexander <akurmazov2001@gmail.com>
2020-11-10 18:00:29 +0300

    Add closest objects interface for inspector client

--- locations/api.py
+++ locations/api.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from scipy import spatial
 
 from locations.serializers import ObjectSerializer
 from locations.models import Object
@@ -8,9 +10,49 @@ class ObjectListAPI(generics.ListAPIView):
     serializer_class = ObjectSerializer
 
     def get_queryset(self):
-        page = self.request.query_params.get('page', None)
+        params = self.request.query_params
+        page = params.get('page', None)
+        queryset = Object.objects.exclude(items__isnull=True)
 
         if page and page.isdigit() and int(page) > 0:
-            return Object.objects.exclude(items__isnull=True)[(int(page) - 1) * 100:int(page) * 100]
+            queryset = queryset[(int(page) - 1) * 100:int(page) * 100]
 
-        return Object.objects.exclude(items__isnull=True)
+        return queryset
+
+
+class CloseObjectsListAPI(generics.RetrieveAPIView):
+
+    def get(self, request, *args, **kwargs):
+        params = self.request.query_params
+        page = params.get('page', None)
+        queryset = Object.objects.exclude(items__isnull=True)
+
+        if page and page.isdigit() and int(page) > 0:
+            queryset = queryset[(int(page) - 1) * 100:int(page) * 100]
+
+        if 'lat' in params and 'long' in params:
+            coordinates = []
+            objects = []
+            for _object in queryset:
+                polygon = _object.geometry.first()
+                coordinates.append(tuple(map(lambda item: float(item), polygon.points.first().coordinates.split(';'))))
+                objects.append(_object)
+
+            user_location = (float(params.get('lat')), float(params.get('long')))
+            tree = spatial.KDTree(coordinates)
+            close_points_ids = tree.query_ball_point(user_location, 2000)
+
+            queryset = []
+            for _id in close_points_ids:
+                object_dict = {
+                    'name': objects[_id].name,
+                    'lat': coordinates[_id][0],
+                    'long': coordinates[_id][1]
+                }
+                queryset.append(object_dict)
+
+        response_dict = {
+            "objects": queryset,
+        }
+
+        return Response(response_dict, status=status.HTTP_200_OK)
--- locations/urls.py
+++ locations/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from locations.api import ObjectListAPI
+from locations.api import ObjectListAPI, CloseObjectsListAPI
 
 
 urlpatterns = [
     path('api/objects/all', ObjectListAPI.as_view(), name='objects-all'),
+    path('api/objects/closest', CloseObjectsListAPI.as_view(), name='objects-closest')
 ]
